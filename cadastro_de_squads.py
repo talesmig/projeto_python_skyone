3693565117e965e2af8bdebf6bceb4b0177736dc
talesmig <tales.mig@hotmail.com>
2022-06-10 17:07:47 -0300

    Adicionando o dev na lista de devs da squad

--- cadastro_de_squads.py
+++ cadastro_de_squads.py
@@ -13,7 +13,7 @@ class Squad:
         def incluir_techlead(self, techlead):
             self.techlead = techlead
 
-            def incluir_dev(self, dev):
+        def incluir_dev(self, dev):
                 self.devs.append(dev)
 
 
@@ -70,6 +70,7 @@ while True:
     cargo_dev = input('Cargo do desenvolvedor: ')
     dev = Dev(nome_dev, fone_dev, cargo_dev)
     dev.incluir_squad(squad)
+    squad.incluir_dev(dev)
 
     option = input('\nDeseja adicionar mais uma squad [S/N]: ')
     if option in 'Nn':
