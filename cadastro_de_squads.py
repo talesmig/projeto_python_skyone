0805e4a0337e4a6193a55ee52bb6cbc10b5d97b6
talesmig <tales.mig@hotmail.com>
2022-06-10 16:22:41 -0300

    Inclusão do techlead na squad que está sendo criada e inclusão da squad ao techlead

--- cadastro_de_squads.py
+++ cadastro_de_squads.py
@@ -37,9 +37,20 @@ nome_squad = input('\nNome da squad: ')
 nome_techlead = input('Nome do techlead da squad: ')
 fone_techlead = input('Telefone do techlead: ')
 
+
 squad = Squad(nome_squad)
 techlead = Colaborador(nome_techlead, fone_techlead)
 
 squads.append(squad)
+
+
+squad = Squad(nome_squad)
+techlead = Colaborador(nome_techlead, fone_techlead)
+squad.incluir_techlead(techlead)
+techlead.incluir_squad(squad)
+
+squad.append(squad)
+
+
 
 
